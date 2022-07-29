05dd3710d14a2088827a28fb4191cc32600800b0
enning94 <79149176+Enning94@users.noreply.github.com>
2022-07-29 06:52:03 +0000

    Create capstone project website

---
+++ render.js
@@ -0,0 +1,47 @@
+
+
+const speakers = [
+    {
+        Name: 'Sarkodie',
+        Rank: 'Ghanaian Rapper',
+        Bio: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit Temporibus neque tempora enim',
+        Featured_image: '<img class="img" src="./img/sarkodie.jpg" alt="" />',
+      
+      },
+    {
+      Name: 'Kendrick Lamar',
+      Rank: 'American Rapper',
+      Bio: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit Temporibus neque tempora enim',
+      Featured_image: ' <img class="img" src="./img/kendrick.jpg" alt="" />',
+    
+    },
+     ];
+
+  
+  function getSpeakers(speakers) {
+    let html = '';
+    for (let i=0; i < speakers.length; i += 1) {
+        html += ` <div class="speaker">
+        <div class="speaker-img">
+        ${speakers[i].Featured_image}
+      </div>
+      <div class="speaker-info">
+      <div class="speaker-name">
+      ${speakers[i].Name}
+      </div>
+      <div class="speaker-rank">
+      ${speakers[i].Rank}
+      </div>
+      <div class="underline3"><hr></div>
+      <div class="speaker-bio">
+      ${speakers[i].Bio}
+      </div>
+    </div>
+    </div>`;
+      }
+
+    document.querySelector('.flx1').innerHTML = html;
+
+  }
+  getSpeakers(speakers);
+
